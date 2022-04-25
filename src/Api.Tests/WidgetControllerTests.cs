5c7c177bb55aaf2cc11fe20d19d0970bf9607e9e
Eric Smith <ydrive65@yahoo.com>
2022-04-25 19:54:30 -0400

    example API test

--- src/Api.Tests/WidgetControllerTests.cs
+++ src/Api.Tests/WidgetControllerTests.cs
@@ -1,7 +1,18 @@
+using Api.Controllers;
+using Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Tests;
 
 public class WidgetControllerTests
 {
+    private readonly WidgetController _sut;
+
+    public WidgetControllerTests()
+    {
+        _sut = new WidgetController(new Data.WidgetContext());
+    }
+
     [Fact]
     public void Create_NewRecord()
     {
@@ -23,7 +34,14 @@ public class WidgetControllerTests
     [Fact]
     public void Get_ReturnsAll()
     {
+        // Act
+        var result = _sut.Get().Result;
 
+        // Assert
+        Assert.NotNull(result);
+        var ok = Assert.IsType<OkObjectResult>(result);
+        var data = Assert.IsType<WidgetDto[]>(ok.Value);
+        Assert.NotEmpty(data);
     }
 
     [Fact]
